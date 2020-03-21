7af3d17662f2225c72275539552c6c2dabafd349
Maxime Dubost <maxime.dubost.75@gmail.com>
2020-03-21 15:56:45 +0100

    Suppression de l'ancien Frontend

--- Frontend/lib/utils/custom_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const LightGrey = Color.fromRGBO(201, 201, 201, 1.0);
-const LightLightGrey = Color.fromRGBO(222, 222, 222, 1.0);
-const VeryLightGrey = Color.fromRGBO(245, 245, 245, 1.0);
-const PrimaryColor = Color.fromRGBO(40, 140, 100, 1.0);
-const SecondaryColor = Color.fromRGBO(20, 120, 80, 1.0);
-const DarkColor = Color.fromRGBO(0, 100, 60, 1.0);
