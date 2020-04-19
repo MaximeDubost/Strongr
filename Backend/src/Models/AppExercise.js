d027eaa9c2c425b1e04ca0c13110aea2cd579433
Kévin <kevtsiii@gmail.com>
2020-04-19 20:20:06 +0200

    Detail update

--- Backend/src/Models/AppExercise.js
+++ Backend/src/Models/AppExercise.js
@@ -1,7 +1,7 @@
 class AppExercise {
     constructor(id, name, muscleList, equipmentList) {
-        this.id_app_exercise = id
-        this.name_app_exercise = name
+        this.id = id
+        this.name = name
         this.muscleList = muscleList
         this.equipmentList = equipmentList
     }
