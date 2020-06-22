d40f8d7c8262281e04baf43425db169dddd37151
Kévin <kevtsiii@gmail.com>
2020-06-22 14:44:37 +0200

    Pour le deploiement

--- Backend/src/core/config/database.js
+++ Backend/src/core/config/database.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg')
-const pool = new Pool(
-    {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: 'root',
-        database: 'Strongit',
+let pool;
+
+if (process.env.NODE_ENV === "production") {
+    pool = new Pool({
+        connectionString: "postgres://mbcxbrjhtvgoht:86df2167086cf0f125e7aa689bbcfc58e6052bc07ff44654e3687a3d898c0597@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/devm9bav7s31bf"
     });
+} else {
+    pool = new Pool(
+        {
+            host: 'localhost',
+            port: 5432,
+            user: 'postgres',
+            password: 'root',
+            database: 'StrongrDB',
+        });
+}
+
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
