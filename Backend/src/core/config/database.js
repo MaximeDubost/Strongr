b51f480fa9eb9442bdc0bba58b5a8d4f955405a4
Kévin <kevtsiii@gmail.com>
2020-07-08 20:56:40 +0200

    Add variable environnement DATABASE_URL

--- Backend/src/core/config/database.js
+++ Backend/src/core/config/database.js
@@ -1,22 +1,20 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 let pool;
 
 if (process.env.NODE_ENV === "production") {
-    pool = new Pool({
-        connectionString: "postgres://mbcxbrjhtvgoht:86df2167086cf0f125e7aa689bbcfc58e6052bc07ff44654e3687a3d898c0597@ec2-176-34-123-50.eu-west-1.compute.amazonaws.com:5432/devm9bav7s31bf"
-    });
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
 } else {
-    pool = new Pool(
-        {
-            host: 'localhost',
-            port: 5432,
-            user: 'postgres',
-            password: 'root',
-            database: 'StrongrDB',
-        });
+  pool = new Pool({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "root",
+    database: "StrongrDB",
+  });
 }
-
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-}
+  query: (text, params) => pool.query(text, params),
+};
