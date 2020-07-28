f25dd903b9a830089f5a6575539d2b7510682191
Maxime Dubost <maxime.dubost.75@gmail.com>
2020-07-28 13:34:30 +0200

    Imports inutiles

--- Frontend/lib/services/UserService.dart
+++ Frontend/lib/services/UserService.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
--- Frontend/lib/views/exercise/exercises_play_view.dart
+++ Frontend/lib/views/exercise/exercises_play_view.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:strongr/models/Exercise.dart';
 import 'package:strongr/models/Status.dart';
 import 'package:strongr/models/Set.dart';
